5836db22c9447e818a5afec58dc3f78c83543b5a
AdaSkribana <marjorie.ramirez@soymas.cl>
2024-02-09 13:12:53 -0300

    version final

--- colabsus.py
+++ colabsus.py
@@ -1,19 +1,23 @@
 from time import sleep
-
+print("******************************************************")
+print("**********Bienvenidos a nuestra bomba virtual*********")
+print("******************************************************")
 for num in reversed(range(11)):
     print(num)
 
     if num == 2:
-        user_option=(input("Ingresa una opcion de entre estos colores para detener la bomba:\n \
-                    'SOLO TIENES UNA OPORTUNIDAD\n rojo \n verde\n \
+        user_option=(input("Ingresa una opcion de entre estos colores para detener la bomba\n \
+                    'SOLO TIENES UNA OPORTUNIDAD'\n \
+                           rojo \n \
+                           verde\n \
                       ¿cual escoges? r: "))
         if user_option == "rojo":
-            print("Te salvaste")
+            print("salvao - bomba desactivada")
             break
         else:
             print("cagaste")
     elif num == 0:
-        print("Kaboom")    
+        print("**bo0m**")    
     sleep(1)  
 
         
